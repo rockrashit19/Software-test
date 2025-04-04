13e3f462c92f7583d36ba4aeba1a72523df9a0f3
rockrashit19 <rd.galiullin@gmail.com>
2025-04-04 16:14:39 +0300

    commented

--- src/DomainModelTest.java
+++ src/DomainModelTest.java
@@ -1,25 +1,25 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
-class DomainModelTest {
-
-    @Test
-    void testHumanOpinion() {
-        Human human = new Human();
-        assertTrue(human.getInventions().contains("колесо"));
-        assertEquals("разумнее дельфинов", human.getOpinion());
-    }
-
-    @Test
-    void testDolphinOpinion() {
-        Dolphin dolphin = new Dolphin();
-        assertEquals("разумнее людей", dolphin.getOpinion());
-    }
-
-    @Test
-    void testOpinionChange() {
-        Human human = new Human();
-        human.updateOpinion(); // Метод, меняющий мнение, если изобретений нет
-        assertNotEquals("дельфины разумнее", human.getOpinion()); // Изобретения есть → мнение не меняется
-    }
-}
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class DomainModelTest {
+//
+//    @Test
+//    void testHumanOpinion() {
+//        Human human = new Human();
+//        assertTrue(human.getInventions().contains("колесо"));
+//        assertEquals("разумнее дельфинов", human.getOpinion());
+//    }
+//
+//    @Test
+//    void testDolphinOpinion() {
+//        Dolphin dolphin = new Dolphin();
+//        assertEquals("разумнее людей", dolphin.getOpinion());
+//    }
+//
+//    @Test
+//    void testOpinionChange() {
+//        Human human = new Human();
+//        human.updateOpinion(); // Метод, меняющий мнение, если изобретений нет
+//        assertNotEquals("дельфины разумнее", human.getOpinion()); // Изобретения есть → мнение не меняется
+//    }
+//}
