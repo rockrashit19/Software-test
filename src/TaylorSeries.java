e97d2bd43d8d843e72e5836aca43f65c495aac2c
rockrashit19 <rd.galiullin@gmail.com>
2025-04-04 18:32:32 +0300

    new TaylorSeries

--- src/TaylorSeries.java
+++ src/TaylorSeries.java
@@ -1,19 +1,25 @@
 public class TaylorSeries {
     public static double calculateCosSeries(double x, int N){
+        if (N <= 0) {
+            return 0.0;
+        }
+
+        x = x % (2 * Math.PI);
+
         double sum = 0.0;
-        for (int n = 0; n < N; n++) {
-            double term = Math.pow(-1, n) * Math.pow(x, 2 * n) / factorial(2 * n);
-            sum += term;
+        double term = 1.0;
+        sum = sum + term;
+        for (int n = 1; n < N; n++) {
+            term = -term * x * x / ((2 * n) * (2 * n - 1));
+            sum = sum + term;
         }
         return sum;
     }
-    private static long factorial(int n){
-        if(n == 0) return 1;
-        long result = 1;
-        for (int i = 1; i <= n ; i++) {
-            result *= i;
-        }
-        return result;
+
+    public static void main(String[] args){
+        double x = 1.0;
+        int n = 11;
+        System.out.println(calculateCosSeries(x, n));
     }
 }
 
