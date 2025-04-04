6fb3462016dca98c0c45fe6f7392b5393cc30ea0
rockrashit19 <rd.galiullin@gmail.com>
2025-04-04 19:19:01 +0300

    add Reason

--- src/Reason.java
+++ src/Reason.java
@@ -1,2 +1,25 @@
 public class Reason {
+    private final ReasonType type;
+    private final String description;
+
+    public Reason(ReasonType type, String description) {
+        this.type = type;
+        this.description = description;
+    }
+
+    public ReasonType getType() {
+        return type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "Reason{" +
+                "type=" + type +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
